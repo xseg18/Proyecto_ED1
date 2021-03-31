fa1d02369e911245801dae032e4d078672a5f44a
J01Gonza <juanp01.gh@gmail.com>
2021-03-31 14:45:43 -0600

    Pacienta class and Singleton modifications

--- Models/Data/Singleton.cs
+++ Models/Data/Singleton.cs
@@ -7,5 +7,40 @@ namespace Proyecto_ED1.Models.Data
 {
     public class Singleton
     {
+        private readonly static Singleton _instance = new Singleton();
+        public E_Arboles.AVL<string, int> Nombre;
+        private readonly static Singleton _instance1 = new Singleton();
+        public E_Arboles.AVL<string, int> Apellido;
+        private readonly static Singleton _instance2 = new Singleton();
+        public E_Arboles.AVL<int, int> CUI;
+
+        private Singleton()
+        {
+            Nombre = new E_Arboles.AVL<string, int>();
+            Apellido = new E_Arboles.AVL<string, int>();
+            CUI = new E_Arboles.AVL<int, int>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        public static Singleton Instance1
+        {
+            get
+            {
+                return _instance1;
+            }
+        }
+        public static Singleton Instance2
+        {
+            get
+            {
+                return _instance2;
+            }
+        }
     }
 }
--- Models/Pacient.cs
+++ Models/Pacient.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_ED1.Models
 {
     public class Pacient
     {
+        [Display(Name ="Nombre")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Apellido")]
+        [Required]
+        public string LName { get; set; }
+        [Display(Name = "DPI/Partida de Nacimiento")]
+        [Required]
+        public int CUI { get; set; }
+        [Display(Name = "Departamento")]
+        [Required]
+        public string Departamento { get; set; }
+        [Display(Name = "Municipio")]
+        [Required]
+        public string Municipio { get; set; }
+        [Display(Name = "Prioridad")]
+        [Required]
+        public int Priority { get; set; }
     }
 }
